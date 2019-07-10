76901a73a2c095e1a58981f0f8297c0e46a78a61
ylochman <lochman.y@gmail.com>
2019-07-10 15:22:46 +0300

    Optimize ORES

--- xml_to_csv_ores.py
+++ xml_to_csv_ores.py
@@ -1,11 +1,8 @@
 import tqdm
 import pandas as pd
 from lxml import etree
-import resource
-
 import mwapi
-import mwtypes
-import requests
+from ores_utils import fetch_wp10_score
 
 def strip_tag_name(t):
     t = t.tag
@@ -14,32 +11,19 @@ def strip_tag_name(t):
         t = t[idx + 1:]
     return t
 
-def score2sum(score_doc, weights):
-    weighted_sum = 0
-    for cl, proba in score_doc['probability'].items():
-        weighted_sum += weights[cl] * proba
-    return weighted_sum
-
-def fetch_wp10_score(rev_id, lang):
-    response = requests.get('https://ores.wikimedia.org/v3/scores/{}wiki/{}/wp10'.format(lang, rev_id))
-    try:
-        return response.json()['enwiki']['scores'][str(rev_id)]['wp10']['score']
-    except:
-        return None
-
-
 class PageParser(object):
-    def __init__(self, lang="en", fetch_ores=True):
-        self.isPage = False
-        self.pages = []
-        self.current_page = {}
+    def __init__(self, lang="en", ores=True, csv=None):
+        if csv is not None:
+            self.csv = csv
+            self.df = pd.read_csv(self.csv)
+        else:
+            self.isPage = False
+            self.pages = []
+            self.current_page = {}
         self.lang = lang
-        self.fetch_ores = fetch_ores
-        self.finalize = self.finalize_page_with_ores if self.fetch_ores \
-                        else self.finalize_page
-        self.ores_weights = {'Stub': 1, 'Start': 2, 'C': 3, 'B': 4, 'GA': 5, 'FA': 6}
+        self.ores = ores
+        # self.ores_weights = {'Stub': 1, 'Start': 2, 'C': 3, 'B': 4, 'GA': 5, 'FA': 6}
         self.session = mwapi.Session("https://{}.wikipedia.org".format(self.lang))
-        self.rvlimit = 100
     
     def handle(self, event, element):
         stipped_elem = strip_tag_name(element)
@@ -47,7 +31,7 @@ class PageParser(object):
             self.isPage = True
         elif event == "end" and stipped_elem == "page":
             self.isPage = False
-            self.finalize()
+            self.finalize_page()
         
         if event == "end" and self.isPage:
             self.parse_page_element(element)
@@ -59,40 +43,48 @@ class PageParser(object):
                         for child in element if child.tail]
             self.current_page[stripped_tag] = " ".join(texts)
 
-    def get_revision(self):
-        for response_doc in self.session.get(action='query', prop='revisions',
-                                             titles=self.current_page["title"],
-                                             rvprop=['ids', 'timestamp'],
-                                             rvlimit=self.rvlimit, rvdir="older", 
-                                             formatversion=2, continuation=True):
-            rev_docs = pd.DataFrame(response_doc['query']['pages'][0]['revisions'])
-            rev = rev_docs[rev_docs.timestamp == self.current_page["timestamp"]]
-            if len(rev) == 1:
-                rev_id = rev["revid"].values[0]
-                try:
-                    return score2sum(fetch_wp10_score(rev_id, self.lang), self.ores_weights)
-                except:
-                    return None
-        return None
-
-    def finalize_page_with_ores(self):
-        self.current_page["ORES"] = self.get_revision()
-        self.pages.append(self.current_page)
-        self.current_page = {}
-
     def finalize_page(self):
         self.pages.append(self.current_page)
         self.current_page = {}
+
+    def finalize(self):
+        self.df = pd.DataFrame(self.pages)
+        if self.ores:
+            self.fetch_ores()
+
+    def fetch_ores(self, chunks=50):
+        print("Fetching ORES")
+        final = pd.DataFrame()
+        for i in tqdm.tqdm(range(0, len(self.df), chunks)):
+            response_doc = next(self.session.get(action="query", prop="revisions",
+                                                titles=self.df["title"].iloc[i:i+chunks],
+                                                formatversion=2, continuation=True))
+            response_doc = pd.DataFrame(response_doc["query"]["pages"])[["title", "revisions"]]
+            revision_fn = lambda x: str(x[0]["revid"])
+            response_doc["revid"] = response_doc.revisions.apply(revision_fn)
+            scores_df = fetch_wp10_score(response_doc["revid"], self.lang)
+            final = pd.concat([final, response_doc[["title","revid"]].join(scores_df, on="revid").set_index("title")])
+        self.df = self.df.join(final, on="title")
+
 
 if __name__ == "__main__":
     XML_FILE = "xml_data/sample.xml"
     CSV_OUTPUT = "csv_data/sample-output.csv"
 
-    page_parser = PageParser(lang="en", fetch_ores=True)
+    CSV_READY = True
+    CSV_OUTPUT_WITHOUT_ORES = "csv_data/sample-output-0.csv"
+    
+    if CSV_READY:
+        page_parser = PageParser(lang="en", csv=CSV_OUTPUT_WITHOUT_ORES)
+        page_parser.fetch_ores()
+        page_parser.df.to_csv(CSV_OUTPUT)
 
-    for i, (event, elem) in tqdm.tqdm(enumerate(etree.iterparse(XML_FILE, \
-                                                    events=('start', 'end')))):
-        page_parser.handle(event, elem)
+    else:
+        page_parser = PageParser(lang="en")
 
-    df = pd.DataFrame(page_parser.pages)
-    df.to_csv(CSV_OUTPUT)
+        for i, (event, elem) in tqdm.tqdm(enumerate(etree.iterparse(XML_FILE, \
+                                                        events=('start', 'end')))):
+            page_parser.handle(event, elem)
+
+        page_parser.finalize(ores=True)
+        page_parser.df.to_csv(CSV_OUTPUT)
