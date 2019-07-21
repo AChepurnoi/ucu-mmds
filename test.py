da130e5a7f76e9529d329ed676478482a9e60a69
Vadym Korshunov <vaden4d@gmail.com>
2019-07-21 21:12:07 +0300

    add evaluation script

--- test.py
+++ test.py
@@ -1,9 +1,155 @@
 import os
+import argparse
+
+import findspark
+try:
+    findspark.init()
+except:
+    PYSPARK_PATH = '../spark/spark-2.4.3-bin-hadoop2.7/'
+    # change path to yours
+    findspark.init(PYSPARK_PATH)
+import pyspark
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+from pyspark.sql.functions import UserDefinedFunction
+
+import sys
+stages = ["1_data_collection", "2_feature_engineering", "3_modeling", "4_evaluation"]
+for stage in stages:
+    sys.path.insert(0, stage)
+
+from functions import extract_features
+
+import bs4
+import requests
+import re
+import numpy as np
+import pandas as pd
+
+def get_raw_article(title):
+    # title formatting
+    title = re.sub('^ {1,}| {1,}$', '', title)
+    title = re.sub(' {1,}', '_', title)
+    # get link with wiki raw text with syntax
+    link = 'https://en.wikipedia.org/w/index.php?title={title}&action=edit'.format(title=title)
+    # parse page and find the needed field
+    response = requests.get(link)
+    html = bs4.BeautifulSoup(response.text, 'html.parser')
+    # get text
+    try:
+        text = html.select('textarea')[0].contents[0]
+        return text
+    except IndexError:
+        print('Error! Try to rewrite your arctile title (maybe you did mistake)')
+        return None
+
+def get_revision_id(title):
+    # title formatting
+    title = re.sub('^ {1,}| {1,}$', '', title)
+    title = re.sub(' {1,}', '_', title)
+    # get link with wiki raw text with syntax
+    link = 'https://en.wikipedia.org/w/index.php?title={title}&action=history'.format(title=title)
+    # parse page and find the needed field
+    response = requests.get(link)
+    html = bs4.BeautifulSoup(response.text, 'html.parser')
+    # get the lastest revision
+    tag = html.findAll(lambda tag: tag.name == "li" and "data-mw-revid" in tag.attrs)[0]
+    revision_id = int(tag.attrs['data-mw-revid'])
+    return revision_id
+
+def get_ores_score(revision_id, lang="en"):
+    wiki = "{}wiki".format(lang)
+    # with CodeTimer("ORES HTTP Request"):
+    response = requests.get("http://ores.wmflabs.org/v3/scores/{}/?models=draftquality|wp10&revids={}".format(wiki, revision_id))
+    try:
+        scores = response.json()[wiki]['scores'][str(revision_id)]['wp10']['score']['probability']
+        return scores
+    except Exception as e:
+        print(f"Alert: Exception fetching ORES score for revision. Ex: {e}")
+        return pd.DataFrame(np.nan, index=revision_ids, columns=['B', 'C', 'FA', 'GA', 'Start', 'Stub'])
+
+def get_features(ores_scores, text, title):
+    # title formatting
+    title = re.sub('^ {1,}| {1,}$', '', title)
+    title = re.sub(' {1,}', ' ', title)
+    # format output dict
+    ores_scores['title'] = title
+    ores_scores['text'] = text
+    # get dataframe
+    pandas_df = pd.DataFrame.from_records([ores_scores])
+    # convert to pyspark dataframe
+    df = spark.createDataFrame(pandas_df.astype(str))
+    pyspark_features = extract_features(df, filter=True, debug=False)
+    pdf = pyspark_features.toPandas()
+    # get pandas data
+    return pdf
+
+ores_scores_dict = {
+    0: 'Stub',
+    1: 'Start',
+    2: 'C',
+    3: 'B',
+    4: 'GA',
+    5: 'FA'
+}
+
+ores_to_descr = {
+    'Stub': '- this article provides very little meaningful content',
+    'Start': '- this article provised some meaningful content, but most readers will need more',
+    'C': '- this article has a middle level of content and references',
+    'B': '- this article has a good amount of content and references',
+    'GA': '- this article very useful for readers: a fairly complete treatment of the subject',
+    'FA': '- this is featured article: professional, outstanding and thorough'
+}
+
+clusters_to_descr = {
+    0: '- the page is pretty well cited and argumented',
+    1: '- the page is medium cited and argumented',
+    2: '- the page is poorly cited and argumented'
+}
+
+references_to_descr = {
+    0: 'books',
+    1: 'scientific papers (journals, publications, etc)',
+    2: 'internet resources (news, archive, etc)',
+    3: 'media materials (prints)'
+}
+
+def formatted_output(pandas_features):
+
+    output = ''
+    #output = '{clustering_output}\n'
+    # add clustering ...
+    # use clusters_to_desct template
+
+    # compute references distribution and create template
+    references = ['book_citations',
+                  'journal_citations',
+                  'web_citations',
+                  'news_citations']
+    references_distr = pandas_features[references].values
+    indexes = (-references_distr).argsort()[0]
+    references_distr = 100 * references_distr[0] / references_distr.sum()
+
+    output += '- the references distribution:\n'
+    for index in indexes:
+        output += '  > {perc:4.0f}% {d}\n'.format(perc=references_distr[index], d=references_to_descr[index])
+
+    # add row with ores info
+    ores_max_index = pandas_features[['Stub', 'Start', 'C', 'B', 'GA', 'FA']].values.argmax()
+    output += '{}'.format(ores_to_descr[ores_scores_dict[ores_max_index]])
+
+    return output
 
 if __name__ == "__main__":
-    import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--title", type=str, required=True)
     args = parser.parse_args()
 
-    print(args.title)
+    text = get_raw_article(args.title)
+    revision_id = get_revision_id(args.title)
+    ores_score = get_ores_score(revision_id)
+    pandas_features = get_features(ores_score, text, args.title)
+    output = formatted_output(pandas_features)
+    print(output)
